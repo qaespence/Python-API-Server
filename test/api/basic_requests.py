47f09dd82d6d970d44383e78870e7dca78d1883e
Eric Spence <ericspence@gmail.com>
2024-09-03 02:38:38 -0700

    Added upload test data files; updated basic_requests post function for file uploads

--- test/api/basic_requests.py
+++ test/api/basic_requests.py
@@ -2,21 +2,30 @@ import requests
 from test.helpers.utils import load_config
 
 
-def post(endpoint: str, payload: dict, headers: dict):
+def post(endpoint: str, payload: dict = None, headers: dict = None, files: dict = None):
     """
     Sends a POST request to the specified endpoint with the given payload and headers.
 
     Args:
         endpoint (str): The API endpoint to send the request to.
-        payload (dict): The data to be sent in the body of the request.
-        headers (dict): The headers to include in the request.
+        payload (dict): (optional) The data to be sent in the body of the request.
+        headers (dict): (optional) The headers to include in the request.
+        files (dict): (optional) The files to include in the request.
 
     Returns:
         response: The response object returned by the requests library.
     """
     config = load_config()
-    response = requests.post(f"{config['base_url']}"+endpoint, json=payload,
-                             headers=headers)
+    url = f"{config['base_url']}{endpoint}"
+
+    # Decide whether to include json or data in the request
+    if payload and not files:
+        response = requests.post(url, json=payload, headers=headers)
+    elif files:
+        response = requests.post(url, files=files, headers=headers, data=payload)
+    else:
+        response = requests.post(url, headers=headers)
+    print(files)
     return response
 
 
